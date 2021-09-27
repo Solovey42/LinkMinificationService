6463ef8e5288796635278f4065a012a49f2dc2c2
Slava <solovey.1999@mail.ru>
2021-09-28 01:09:38 +0700

    Creating data class and context for database

---
+++ Domain/Link.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string LongAddress { get; set; }
+        public string ShortAddress { get; set; }
+    }
+}
---
+++ Domain/LinksContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class LinksContext : DbContext
+    {
+        public DbSet<Link> Links { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=LinksDatabase.db");
+        }
+        public LinksContext(DbContextOptions<LinksContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
